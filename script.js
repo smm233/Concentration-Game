05f7f1a454b6e745cdd60afece5c2b149733ae46
shil-mil <shyshy122303@gmail.com>
2025-03-16 13:03:58 +0800

    Create functions to shuffle cards, start game, and hide cards

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var errors = 0;
+var rows = 3;
+var columns = 6;
 var cards = [
     "Beidou",
     "Clorinde",
     "Dehya",
-    "Furina",
+    // "Furina",
     "KujouSara",
     "Navia",
     "Ningguang",
@@ -13,6 +15,8 @@ var cards = [
 ];
 
 var cardSet;
+var cardSelected;
+var frontSelected;
 var board = [];
 
 window.onload = function() {
@@ -22,6 +26,7 @@ window.onload = function() {
 
 function shuffleCards() {
     cardSet = cards.concat(cards);
+    console.log(cardSet);
     for (let i = 0; i < cardSet.length; i++) {
         let j = Math.floor(Math.random() * cardSet.length);
 
@@ -29,8 +34,36 @@ function shuffleCards() {
         cardSet[i] = cardSet[j];
         cardSet[j] = temp;
     }
+    console.log(cardSet);
 }
 
 function startGame() {
-    
+    for (let i = 0; i < rows; i++) {
+        let row = [];
+        for (let j = 0; j < columns; j++) {
+            let cardImg = cardSet.pop();
+            row.push(cardImg);
+
+            let card = document.createElement("img");
+            card.id = i.toString() + "-" + j.toString();
+            card.src = "images/" + cardImg + ".png";
+            card.classList.add("card");
+            card.addEventListener("click", selectCard());
+            console.log(card.src);
+            document.getElementById("board").append(card);
+            
+        }
+        board.push(row);
+    }
+    console.log(board);
+    setTimeout(hideCards, 1500);
+}
+
+function hideCards() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let card = document.getElementById(i.toString() + "-" + j.toString());
+            card.src = "images/back.png";
+        }
+    }
 }
